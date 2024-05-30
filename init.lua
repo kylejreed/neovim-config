ac74f424ea19769682b611705ead914edf719974
Kyle Reed <kylejreed2@gmail.com>
2024-05-30 06:22:06 -0700

    change theme

--- init.lua
+++ init.lua
@@ -532,6 +532,7 @@ require('lazy').setup({
         -- gopls = {},
         tailwindcss = {},
         pyright = {},
+        -- gleam = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
